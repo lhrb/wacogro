3a91acad968ea39c6fc49d8b6fcd48e3083fc9ec
lukas <lukas.block@pm.me>
2023-01-23 00:07:42 +0100

    remove complexity metric code

--- src/dev/lhrb/wacogro.clj
+++ src/dev/lhrb/wacogro.clj
@@ -4,66 +4,23 @@
    [clojure.java.io :as io]
    [clojure.string :as str]
    [clojure.java.shell :as sh]
-   [criterium.core :as c])
+   [criterium.core :as c]
+   [taoensso.nippy :as nippy])
   (:import (java.io File)))
 
-(defn greet
-  "Callable entry point to the application."
-  [data]
-  (println (str "Hello, " (or (:name data) "World") "!")))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (greet {:name (first args)}))
 
-;; --------------- fsm ------------------
+;; ---------------------------------------  de/serialize  ---------------------------------------
 
-(defn start-state []
-  {:state :start
-   :lines 0
-   :spaces 0})
+(defn save-to-file! [data]
+  (let [name (str "resources/"
+                  (.format (java.text.SimpleDateFormat. "yyyy-MM-dd")
+                           (java.util.Date. ))
+                  ".edn")]
+    (nippy/freeze-to-file (io/file name) data)))
 
-(defn start
-  [^long token state]
-  (case token
-    9  (update state :spaces #(+ 4 %))  ;; tab
-    32 (update state :spaces inc)
-    10 (update state :lines inc)
-    (assoc state :state :skip)))
-
-(defn skip
-  [^long token state]
-  (if (== 10 token)
-    (-> state (update :lines inc) (assoc :state :start))
-    state))
-
-(defn transition [k ^long token state]
-  (case k
-    :start (start token state)
-    :skip (skip token state)))
-
-(defn cpx-file
-  [^File f]
-  (with-open [rdr (io/reader f)]
-    (loop [current (start-state)
-           token (.read rdr)]
-      (if (== -1 token)
-        (-> current (dissoc :state) (assoc :name (str f)))
-        (recur (transition (get current :state) token current)
-               (.read rdr))))))
-
-(defn file-extension? [extension]
-  (fn [^File f]
-    (str/ends-with? (.getName f) extension)))
-
-(defn analyze-all
-  [path]
-  (->> (file-seq (io/file path))
-       (filter (file-extension? ".java"))
-       (pmap cpx-file)
-       (into [])))
-
+(defn read-file [name]
+  (nippy/thaw-from-file (io/file name)))
 
 (comment
 
@@ -80,14 +37,18 @@
 
   (str/split-lines (:out (sh/sh (str "/usr/bin/git -C " dir " status"))))
 
-  (sh/sh "git" "log")
+  (sh/sh "git" "-C" "../elasticsearch/" "log")
 
+
+  (apply sh/sh (str/split "git log" #" "))
 
   (def stats (analyze-all "../elasticsearch/"))
 
   (->> stats
        (sort-by :spaces >)
        (take 10))
+
+  (sh/sh "git log --since='3 weeks ago' --format=format: --name-only | grep -Ev '^$' | sort | uniq")
 
   (require '[kixi.stats.core :as kixi])
 
